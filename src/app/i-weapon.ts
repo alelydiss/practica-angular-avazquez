5ed92e18f06f1b17bb7c8e7469db6706700b2133
alelydiss <alelydiss@gmail.com>
2025-03-06 16:32:58 +0100

    Actualizacion interfaz

--- src/app/i-weapon.ts
+++ src/app/i-weapon.ts
@@ -1,27 +1,20 @@
 export interface IWeapon {
-    uuid: string;
-    displayName: string;
-    category: string;
-    displayIcon: string;
-    weaponStats: {
-      fireRate: number;
-    };
-    shopData: {
-      cost: number;
-    };
-    skins: Skin[];
-  }
-  
-  export interface Skin {
-    uuid: string;
-    displayName: string;
-    displayIcon: string;
-    chromas: Chroma[];
-  }
-  
-  export interface Chroma {
-    displayName: string;
-    fullRender: string;
-    streamedVideo: string;
-  }
-  
+  uuid: string;
+  displayName: string;
+  category: string;
+  displayIcon: string;
+  weaponStats?: {
+    fireRate: number;
+    magazineSize: number;
+  };
+  shopData?: {
+    cost: number;
+  };
+  skins: ISkin[];
+}
+
+export interface ISkin {
+  uuid: string;
+  displayName: string;
+  displayIcon?: string;
+}
