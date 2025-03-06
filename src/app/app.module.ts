4b8cf3bc74981f6b3831bce52e176ddcbf361e85
alelydiss <alelydiss@gmail.com>
2025-03-06 09:25:50 +0100

    Creacion del servicio para la busqueda en la api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideHttpClient } from '@angular/common/http';
+import { AcercaComponent } from './acerca/acerca.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
-import { AcercaComponent } from './acerca/acerca.component';
 import { PaginaNoEncontradaComponent } from './pagina-no-encontrada/pagina-no-encontrada.component';
 
 @NgModule({
@@ -20,7 +21,7 @@ import { PaginaNoEncontradaComponent } from './pagina-no-encontrada/pagina-no-en
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
