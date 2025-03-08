eb5a1ff9f23f5db96ea424e0fc9b518950fc02fc
alelydiss <alelydiss@gmail.com>
2025-03-08 15:31:02 +0100

    Finalizacion del apartado de bienvenida

--- src/app/bienvenida/bienvenida.component.ts
+++ src/app/bienvenida/bienvenida.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ValorantService } from '../valorant.service';
 
 @Component({
   selector: 'app-bienvenida',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './bienvenida.component.css'
 })
 export class BienvenidaComponent {
+  agents: any[] = [];
+  filteredAgents: any[] = [];
+  selectedRole: string = '';
 
+  constructor(private valorantService: ValorantService) {}
+
+  ngOnInit(): void {
+    this.valorantService.getAgents().subscribe((agents) => {
+      this.agents = agents.filter((agent: any) => agent.isPlayableCharacter);
+      this.filteredAgents = [...this.agents];
+    });
+  }
+  
+
+  getAgentGradient(agent: any): string {
+    return agent.backgroundGradientColors
+      ? `linear-gradient(135deg, #${agent.backgroundGradientColors[0]}, #${agent.backgroundGradientColors[1]})`
+      : '#333';
+  }
+  
+  filterAgents(role: string): void {
+    this.selectedRole = role;
+    this.filteredAgents = role === '' ? this.agents : this.agents.filter(agent => agent.role?.displayName === role);
+  }
 }
+
--- src/app/valorant.service.ts
+++ src/app/valorant.service.ts
@@ -7,27 +7,30 @@ import { ISkin, IWeapon } from './i-weapon';
   providedIn: 'root'
 })
 export class ValorantService {
-  private apiUrl = 'https://valorant-api.com/v1/weapons';
+  private weaponsUrl = 'https://valorant-api.com/v1/weapons';
+  private agentsUrl = 'https://valorant-api.com/v1/agents';
 
   constructor(private http: HttpClient) {}
 
   getWeapons(): Observable<{ data: IWeapon[] }> {
-    return this.http.get<{ data: IWeapon[] }>(this.apiUrl);
+    return this.http.get<{ data: IWeapon[] }>(this.weaponsUrl);
   }
 
   getSkinsByWeapon(weaponUuid: string): Observable<ISkin[]> {
-  return this.http.get<{ data: { skins: ISkin[] } }>(`https://valorant-api.com/v1/weapons/${weaponUuid}`)
-    .pipe(map(response => response.data.skins))
-}
+    return this.http.get<{ data: { skins: ISkin[] } }>(`${this.weaponsUrl}/${weaponUuid}`)
+      .pipe(map(response => response.data.skins));
+  }
 
-getSkinById(skinId: string): Observable<ISkin> {
-  return this.http.get<{ status: number; data: ISkin }>(
-    `https://valorant-api.com/v1/weapons/skins/${skinId}`
-  ).pipe(
-    map(response => response.data)
-  );
-}
+  getSkinById(skinId: string): Observable<ISkin> {
+    return this.http.get<{ status: number; data: ISkin }>(
+      `https://valorant-api.com/v1/weapons/skins/${skinId}`
+    ).pipe(map(response => response.data));
+  }
 
-
+  getAgents(): Observable<any[]> {
+    return this.http.get<{ data: any[] }>(this.agentsUrl).pipe(
+      map(response => response.data.filter((agent: any) => agent.isPlayableCharacter))
+    );
+  }
   
 }
